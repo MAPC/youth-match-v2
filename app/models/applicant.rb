84ae68039072990701f4a794b750ebcc368e4f96
Matthew Zagaja <mzagaja@alum.wpi.edu>
2017-02-09 19:52:47 -0500

    Provide test data and a grid cell
    This commit updates the intersects method in Box to accept a location as well as a lat,long text string. This comports with how the method was used in the previous application but also lets us manually test it from the controller. I have also transplanted the test data and the methods that calculate the grid cell for each applicant or position location.

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -1,2 +1,10 @@
 class Applicant < ApplicationRecord
+  before_validation :compute_grid_id, if: 'location.present?'
+
+  private
+
+  def compute_grid_id
+    grid = Box.intersects(location: location)
+    self.grid_id = grid.g250m_id if grid
+  end
 end
--- app/models/box.rb
+++ app/models/box.rb
@@ -1,9 +1,14 @@
 class Box < ApplicationRecord
   def self.intersects(latlong)
-    raise 'please format as lat,long' if latlong.exclude?(',') || latlong.blank?
-    latlong = latlong.split(",")
-    location = RGeo::Geographic.spherical_factory(srid: 4326)
-                               .point(latlong[1], latlong[0])
+    raise 'please provide a location' if latlong.blank?
+    if latlong.is_a? String
+      raise 'please format as lat,long' if latlong.exclude?(',')
+      latlong = latlong.split(',')
+      location = RGeo::Geographic.spherical_factory(srid: 4326)
+                                 .point(latlong[1], latlong[0])
+    else
+      location = latlong[:location]
+    end
     # Why doesn't #{location} work here and I have to use point: location?
     where('ST_Intersects(geom, :point)', point: location).first
   end
