7f20f9d641561c076a0f865722848300dbcab603
Matthew Zagaja <mzagaja@alum.wpi.edu>
2017-03-13 17:11:04 -0400

    Fix Name of Import Positions Controller
    This changes the name of the import positions controller to more logically describe the fact that it is an object that might hold things like status, etc. for imports of positions by renaming it to PositionImportsController. This way a user might POST to create a PositionImport and then do other things with an import.

--- app/controllers/import_positions_controller.rb
+++
@@ -1,7 +0,0 @@
-class ImportPositionsController < ApplicationController
-  skip_before_action :verify_authenticity_token
-  def create
-    ImportPositionsJob.perform_now
-    render body: 'Positions will be imported from ICIMs'
-  end
-end
