ba6d7f0e1a02bb01d16ff42e588402042bd8b954
Matthew Zagaja <mzagaja@alum.wpi.edu>
2017-05-04 13:53:47 -0400

    Check status of applicant before updating
    We need to check to make sure an applicant is in site selection status before changing them back to new submission status.

--- app/jobs/update_rejected_applicants_job.rb
+++ app/jobs/update_rejected_applicants_job.rb
@@ -3,7 +3,7 @@ class UpdateRejectedApplicantsJob < ApplicationJob
 
   def perform(*args)
     not_chosen_applicants.each do |applicant|
-      update_applicant_to_new_submission(applicant)
+      update_applicant_to_new_submission(applicant) if status_is_site_selection?(applicant)
     end
   end
 
@@ -11,14 +11,19 @@ class UpdateRejectedApplicantsJob < ApplicationJob
 
   def not_chosen_applicants
     chosen_applicants = []
-    chosen_applicants << Pick.where(status: :hire).pluck(:applicant_id)
-    chosen_applicants << Requisition.where(status: :hire).pluck(:applicant_id)
+    chosen_applicants << Pick.all.pluck(:applicant_id)
     chosen_applicants.flatten!
     not_chosen_applicants = Applicant.all.pluck(:id) - chosen_applicants
     Applicant.find(not_chosen_applicants)
   end
 
+  def status_is_site_selection?(applicant)
+    response = icims_get(object: 'applicantworkflows', id: applicant.workflow_id)
+    response['status']['id'] == 'C2028' ? true :false
+  end
+
   def update_applicant_to_new_submission(applicant)
+    sleep 1
     Rails.logger.info "Updating Applicant iCIMS ID #{applicant.icims_id} to new submission: #{applicant.id}"
     response = Faraday.patch do |req|
       req.url 'https://api.icims.com/customers/7383/applicantworkflows/' + applicant.workflow_id.to_s
@@ -30,5 +35,12 @@ class UpdateRejectedApplicantsJob < ApplicationJob
       Rails.logger.error 'ICIMS Update Status to New Submission Failed for: ' + applicant.icims_id.to_s
       Rails.logger.error 'Status: ' + response.status.to_s + ' Body: ' + response.body
     end
+  end
+
+  def icims_get(object:, fields: '', id:)
+    response = Faraday.get("https://api.icims.com/customers/7383/#{object}/#{id}",
+                           { fields: fields },
+                           authorization: "Basic #{Rails.application.secrets.icims_authorization_key}")
+    JSON.parse(response.body)
   end
 end
