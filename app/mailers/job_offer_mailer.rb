5e8b3e4baf7226a202dd2d868a01f5e1554d6304
Matthew Zagaja <mzagaja@alum.wpi.edu>
2017-05-17 17:35:40 -0400

    Use server URL for applicant test
    Uses the url of the current server for applicant email links instead of a hard coded url.

--- app/mailers/job_offer_mailer.rb
+++ app/mailers/job_offer_mailer.rb
@@ -5,8 +5,8 @@ class JobOfferMailer < ApplicationMailer
     @user = user
     @offer = user.applicant.offer
     @position = user.applicant.offer.position
-    @accept_url  = 'http://dyee.mapc.org/offers/accept?email=' + user.email + '&token=' + user.authentication_token
-    @decline_url = 'http://dyee.mapc.org/offers/decline?email=' + user.email + '&token=' + user.authentication_token
+    @accept_url  = root_url + 'offers/accept?email=' + user.email + '&token=' + user.authentication_token
+    @decline_url = root_url + 'offers/decline?email=' + user.email + '&token=' + user.authentication_token
     mail(to: @user.email, subject: '2017 Successlink Lottery Job Offer - We’ve picked you for a summer job!')
   end
 end
