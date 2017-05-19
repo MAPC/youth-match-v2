785fad9256fa5b2e4a98f2a87ff87ba21decb582
Matthew Zagaja <mzagaja@alum.wpi.edu>
2017-05-19 15:54:08 -0400

    Update lottery activated candidates job

---
+++ app/jobs/update_lottery_activated_candidates_job.rb
@@ -0,0 +1,31 @@
+class UpdateLotteryActivatedCandidatesJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    Applicant.all.each do |applicant|
+      update_applicant_to_lottery_activated(applicant) if status_is_new_submission?(applicant)
+    end
+  end
+
+  private
+
+  def status_is_new_submission?(applicant)
+    response = icims_get(object: 'applicantworkflows', id: applicant.workflow_id)
+    Rails.logger.info response['status']['id'].to_s
+    response['status']['id'] == 'D10100' ? true : false
+  end
+
+  def update_applicant_to_lottery_activated(applicant)
+    Rails.logger.info "Updating Applicant iCIMS ID #{applicant.icims_id} to lottery activated: #{applicant.id}"
+    response = Faraday.patch do |req|
+      req.url 'https://api.icims.com/customers/7383/applicantworkflows/' + applicant.workflow_id.to_s
+      req.body = %Q{ {"status":{"id":"C38354"}} }
+      req.headers['authorization'] = "Basic #{Rails.application.secrets.icims_authorization_key}"
+      req.headers["content-type"] = 'application/json'
+    end
+    unless response.success?
+      Rails.logger.error 'ICIMS Update Status to Lottery Activated Failed for: ' + applicant.id.to_s
+      Rails.logger.error 'Status: ' + response.status.to_s + ' Body: ' + response.body
+    end
+  end
+end
