a49115e73ba7661396565beb4cac65eb7d53a9a6
Matthew Zagaja <mzagaja@alum.wpi.edu>
2017-05-21 22:33:24 -0400

    Update applicant to lottery activated

--- app/jobs/update_lottery_activated_from_icims_job.rb
+++ app/jobs/update_lottery_activated_from_icims_job.rb
@@ -4,8 +4,10 @@ class UpdateLotteryActivatedFromIcimsJob < ApplicationJob
   def perform(*args)
     Applicant.all.each do |applicant|
       if status_is_lottery_activated?(applicant)
+        Rails.logger.info 'Updating applicant to lottery activated:' + applicant.icims_id
         applicant.update(lottery_activated: true)
       else
+        Rails.logger.info 'Updating applicant to not lottery activated:' + applicant.icims_id
         applicant.update(lottery_activated: false)
       end
     end
@@ -16,5 +18,12 @@ class UpdateLotteryActivatedFromIcimsJob < ApplicationJob
   def status_is_lottery_activated?(applicant)
     response = icims_get(object: 'applicantworkflows', id: applicant.workflow_id)
     response['status']['id'] == 'C38354' ? true : false
+  end
+
+  def icims_get(object:, fields: '', id:)
+    response = Faraday.get("https://api.icims.com/customers/7383/#{object}/#{id}",
+                           { fields: fields },
+                           authorization: "Basic #{Rails.application.secrets.icims_authorization_key}")
+    JSON.parse(response.body)
   end
 end
