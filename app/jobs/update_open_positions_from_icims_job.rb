3b9dee4ee176794f2c7cbbaa8366e7193968ec25
Matthew Zagaja <mzagaja@alum.wpi.edu>
2017-05-24 16:15:07 -0400

    Make update open positions into a job

---
+++ app/jobs/update_open_positions_from_icims_job.rb
@@ -0,0 +1,40 @@
+class UpdateOpenPositionsFromIcimsJob < ApplicationJob
+  queue_as :default
+
+  def perform(position)
+    position_information = icims_get(object: 'jobs',
+                                      fields: 'numberofpositions',
+                                      id: position.icims_id)
+    # get the number of folks in the four bin/statuses that are excluded:
+    # Selected by Site C2028
+    # Onboarding C23504
+    # Documents Ready C51324
+    # OHR Compliance C23505
+    response = icims_search(type: 'applicantworkflows',
+                            body: %Q{{"filters":[{"name":"applicantworkflow.status","value":["C2028","C23504","C51324","C23505"],"operator":"="},{"name":"applicantworkflow.job.id","value":["#{position.icims_id}"],"operator":"="}],"operator":"&"}})
+    open_position_count = position_information['numberofpositions'].to_i - response['searchResults'].pluck('id').count
+    open_position_count = 0 if open_position_count < 0
+    position.update(open_positions: open_position_count)
+  end
+
+  private
+
+  def icims_get(object:, fields: '', id:)
+    response = Faraday.get("https://api.icims.com/customers/6405/#{object}/#{id}",
+                           { fields: fields },
+                           authorization: "Basic #{Rails.application.secrets.icims_authorization_key}")
+    JSON.parse(response.body)
+  end
+
+  def icims_search(type:, body:)
+    response = Faraday.post do |req|
+      req.url = 'https://api.icims.com/customers/6405/search/' + type
+      req.body = body
+      req.headers['authorization'] = "Basic #{Rails.application.secrets.icims_authorization_key}"
+      req.headers["content-type"] = 'application/json'
+      req.options.timeout = 60
+      req.options.open_timeout = 60
+    end
+    JSON.parse(response.body)
+  end
+end
--- lib/tasks/import.rake
+++ lib/tasks/import.rake
@@ -462,19 +462,7 @@ namespace :import do
   desc 'Update position information from ICIMS'
   task refresh_position_data: :environment do
     Position.all.each do |position|
-      position_information = icims_get(object: 'jobs',
-                                        fields: 'numberofpositions',
-                                        id: position.icims_id)
-      # get the number of folks in the four bin/statuses that are excluded:
-      # Selected by Site C2028
-      # Onboarding C23504
-      # Documents Ready C51324
-      # OHR Compliance C23505
-      response = icims_search(type: 'applicantworkflows',
-                              body: %Q{{"filters":[{"name":"applicantworkflow.status","value":["C2028","C23504","C51324","C23505"],"operator":"="},{"name":"applicantworkflow.job.id","value":["#{position.icims_id}"],"operator":"="}],"operator":"&"}})
-      open_position_count = position_information['numberofpositions'].to_i - response['searchResults'].pluck('id').count
-      open_position_count = 0 if open_position_count < 0
-      position.update(open_positions: open_position_count)
+      UpdateOpenPositionsFromIcimsJob.perform_later(position)
     end
   end
 
