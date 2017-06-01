fc7346db1b45623df52029dbe931ed4b35ca0910
Matthew Zagaja <mzagaja@alum.wpi.edu>
2017-06-01 15:29:02 -0400

    Don’t include “Lottery Placement Accepted”
    Don’t include “Lottery Placement Accepted” status people in the position counts.

--- app/jobs/update_open_positions_from_icims_job.rb
+++ app/jobs/update_open_positions_from_icims_job.rb
@@ -10,8 +10,9 @@ class UpdateOpenPositionsFromIcimsJob < ApplicationJob
     # Onboarding C23504
     # Documents Ready C51324
     # OHR Compliance C23505
+    # Lottery Placement Accepted: C36951
     response = icims_search(type: 'applicantworkflows',
-                            body: %Q{{"filters":[{"name":"applicantworkflow.status","value":["C2028","C23504","C51324","C23505"],"operator":"="},{"name":"applicantworkflow.job.id","value":["#{position.icims_id}"],"operator":"="}],"operator":"&"}})
+                            body: %Q{{"filters":[{"name":"applicantworkflow.status","value":["C2028","C23504","C51324","C23505","C36951"],"operator":"="},{"name":"applicantworkflow.job.id","value":["#{position.icims_id}"],"operator":"="}],"operator":"&"}})
     open_position_count = position_information['numberofpositions'].to_i - response['searchResults'].pluck('id').count
     open_position_count = 0 if open_position_count < 0
     position.update(open_positions: open_position_count)
