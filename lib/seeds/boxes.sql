cddc6a8d2daead6e2a6d224f943cdf802c8d3df6
Matthew Zagaja <mzagaja@alum.wpi.edu>
2017-08-14 15:33:27 -0400

    Resolve #28 Seed Compressed Travel Times
    This fixes #28 by moving to the proprietary postgres data format for dumping and restoring the travel times and boxes tables in our seed.

--- lib/seeds/boxes.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1184dc8dca34bc0aeff8cd4bd2238830f9e7b2f4145c56093e2f46e4609a82b0
-size 2821213
--- lib/seeds/travel_times.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2d79583df669ea664d4fe75107bbdcd5be5d6a0930b488f8d947207936ac46c3
-size 970416964
