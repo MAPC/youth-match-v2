6b95afdb69e8f2dc805e7a40d06be6ddd8502ee4
Matthew Zagaja <mzagaja@alum.wpi.edu>
2017-10-11 09:28:26 -0400

    Fix formatting error

--- app/workers/send_text_worker.rb
+++ app/workers/send_text_worker.rb
@@ -3,10 +3,10 @@ class SendTextWorker
   def perform(to, message)
     begin
       Rails.logger.info 'Hello from SendTextWorker!'
-    client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid,
-                                      Rails.application.secrets.twilio_auth_token
-    client.messages.create from: '6176168535', to: to,
-                           body: message
+      client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid,
+                                        Rails.application.secrets.twilio_auth_token
+      client.messages.create from: '6176168535', to: to,
+                             body: message
     rescue Twilio::REST::RequestError => e
       Rails.logger.error 'Twilio Error: ' + e
     end
