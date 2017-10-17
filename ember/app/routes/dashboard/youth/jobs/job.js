b48f6755dc605a7d01e7d2581cd892224ce6e64c
Eric Youngberg <eyoungberg@mapc.org>
2017-10-17 10:15:36 -0400

    Hookup functionality for adding applicants to positions

--- ember/app/routes/dashboard/youth/jobs/job.js
+++ ember/app/routes/dashboard/youth/jobs/job.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
+import RSVP from 'rsvp';
+//import CenterMapOnGeometry from '../../../../mixins/center-map-on-geometry';
+//import trackPage from '../../../../mixins/track-page';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(/*trackPage, CenterMapOnGeometry.reopen({ hashProperty: 'job' }), */ {
+
+  mapState: Ember.inject.service(),
+
+  model(param) {
+    const youthJobsModel = this.modelFor('dashboard.youth.jobs');
+    const position = youthJobsModel.positions.findBy('id', param.position_id);
+    const user = youthJobsModel.user;
+
+    return RSVP.hash({ position, user });
+  }
+
 });
