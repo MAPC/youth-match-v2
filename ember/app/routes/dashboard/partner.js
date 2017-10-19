d63c32b55110b722fe33c321f1f9123af21770c6
Eric Youngberg <eyoungberg@mapc.org>
2017-10-19 14:30:55 -0400

    Change partner route

--- ember/app/routes/dashboard/partner.js
+++ ember/app/routes/dashboard/partner.js
@@ -1,11 +1,9 @@
 import Ember from 'ember';
 import RSVP from 'rsvp';
 import { flatten } from '../../helpers/flatten';
-import trackPage from '../../mixins/track-page';
-import updateMapBounds from '../../mixins/update-map-bounds';
 
 
-export default Ember.Route.extend(trackPage, {
+export default Ember.Route.extend({
 
   model() {
     let user = this.modelFor('dashboard');
@@ -13,15 +11,15 @@ export default Ember.Route.extend(trackPage, {
     return RSVP.hash({
       user,
       picks: this.store.findAll('pick'),
-      requisitions: user.get('positions').then(positions=> {
-        return RSVP.all(positions.mapBy('requisitions')).then(collection=> {
+      requisitions: user.get('positions').then(positions => {
+        return RSVP.all(positions.mapBy('requisitions')).then(collection => {
           return flatten(collection);
         });
       }),
-      applicants: user.get('positions').then(positions=> {
+      applicants: user.get('positions').then(() => {
         return this.store.findAll('applicant');
       })
-    })
+    });
   },
 
 });
