f680dceb0e5b86e335f2317fe0b7f9d9f680b031
Matthew Zagaja <mzagaja@alum.wpi.edu>
2017-10-24 14:45:51 -0400

    Resolve #164 Send Decline Email
    Update the offer model to send an email to an applicant if they decline their job offer.
    
    Preliminarily Address #172 by changing the URL included in the offer email. However we still need to know the final URL home or figure out a way to get the requesting domain in order to set this dynamically.

--- rails/app/mailers/applicant_mailer.rb
+++ rails/app/mailers/applicant_mailer.rb
@@ -3,7 +3,11 @@ class ApplicantMailer < ApplicationMailer
 
   def job_picker_email(user)
     @user = user
-    @url  = 'http://youthjobs.mapc.org/jobs?email=' + user.email + '&token=' + user.authentication_token
+    @url  = "http://youthjobs.mapc.org/login?email=#{user.email}&token=#{user.authentication_token}"
     mail(to: @user.email, subject: 'Important Next Steps for Successlink Summer Jobs')
+  end
+
+  def decline_offer_email(user)
+    mail(to: user.email, subject: 'Information on your Declined Successlink Offer')
   end
 end
--- rails/spec/mailers/applicant_mailer_spec.rb
+++ rails/spec/mailers/applicant_mailer_spec.rb
@@ -1,5 +1,32 @@
 require "rails_helper"
 
 RSpec.describe ApplicantMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "job_picker" do
+    user = FactoryGirl.create(:user)
+    let(:mail) { ApplicantMailer.job_picker_email(user) }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("Important Next Steps for Successlink Summer Jobs")
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(["youthline@boston.gov"])
+    end
+
+    it "renders the body" do
+      expect(mail.body.encoded).to match("Dear SuccessLink Applicant")
+    end
+  end
+
+  describe "decline_offer" do
+    user = FactoryGirl.create(:user)
+    let(:mail) { ApplicantMailer.decline_offer_email(user) } # command where applicant declines an offer
+    it "renders the headers" do
+      expect(mail.subject).to eq("Information on your Declined Successlink Offer")
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(["youthline@boston.gov"])
+    end
+
+    it "renders the body" do
+      expect(mail.body.encoded).to match("Dear SuccessLink Applicant")
+    end
+  end
 end
