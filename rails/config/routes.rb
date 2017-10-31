aee9ed0afe12a2642dcb86e7d2bd7a70ba63d544
Matthew Zagaja <mzagaja@alum.wpi.edu>
2017-10-31 16:09:14 -0400

    Update expire lottery status endpoint
    This updates the expire lottery status endpoint to the point to the ScheduledSet queue because we learned the job for the lottery expiration sits there before making its way to the expire_lottery queue due to its 72 hour delay.

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -31,7 +31,7 @@ Rails.application.routes.draw do
     resources :position_imports, only: [:create]
     resources :password_resets, only: [:create]
     resources :offer_emails, only: [:create]
-    match "expire-lottery-status" => proc { [200, {"Content-Type" => "text/plain"}, [Sidekiq::Queue.new('expire_lottery').size < 1 ? "Empty" : "Active" ]] }, via: :get
+    match "expire-lottery-status" => proc { [200, {"Content-Type" => "text/plain"}, [Sidekiq::ScheduledSet.new.size < 1 ? "Empty" : "Active" ]] }, via: :get
     match "match-lottery-status" => proc { [200, {"Content-Type" => "text/plain"}, [Sidekiq::Queue.new('match_lottery').size < 1 ? "Empty" : "Active" ]] }, via: :get
     match "workers-status" => proc { [200, {"Content-Type" => "text/plain"}, [Sidekiq::Workers.new.size < 1 ? "Empty" : "Active" ]] }, via: :get
   end
